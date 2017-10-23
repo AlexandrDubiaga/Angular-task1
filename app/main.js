53257b221abaff880d47d243c7408a128ff495f3
student <student@GFL-1486.ad.geeksforless.net>
2017-10-23 19:48:56 +0300

    Ang1/1

--- app/main.js
+++ app/main.js
@@ -1,1 +1,13 @@
-angular.module("app",[])
+angular.module('app', [])
+var app = angular.module('app')
+app.controller('nameController', settingFunc);
+function settingFunc($scope)
+{
+    $scope.names = "Vasia"
+ this.names = "test this"
+ var self = this
+ this.names = []
+ this.addName = function(name){
+     self.names.push(name);
+ };
+ }
