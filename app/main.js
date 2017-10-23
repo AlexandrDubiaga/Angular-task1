049c565f4c184a8191bb9ec791080c08943aae0e
student <student@GFL-1486.ad.geeksforless.net>
2017-10-23 19:54:48 +0300

    Ang1/3

--- app/main.js
+++ app/main.js
@@ -3,7 +3,17 @@ var app = angular.module('app')
 app.controller('nameController', settingFunc);
 function settingFunc($scope)
 {
-    $scope.names = "Vasia"
+    $scope.names = [
+        'Jani',
+        'Carl',
+        'Margareth',
+        'Hege',
+        'Joe',
+        'Gustav',
+        'Birgit',
+        'Mary',
+        'Kai'
+    ];
  this.names = "test this"
  var self = this
  this.names = []
